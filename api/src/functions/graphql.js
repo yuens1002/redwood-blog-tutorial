3c999c9d8ec35f9990919076e8fc34073ea08a2f
sunny yuen <yuens1002@gmail.com>
2020-07-12 17:50:11 -0400

    authenticate user and display login screen

--- api/src/functions/graphql.js
+++ api/src/functions/graphql.js
@@ -7,8 +7,10 @@ import schemas from 'src/graphql/**/*.{js,ts}'
 import services from 'src/services/**/*.{js,ts}'
 
 import { db } from 'src/lib/db'
+import getCurrentUser from 'src/lib/auth'
 
 export const handler = createGraphQLHandler({
+  getCurrentUser,
   schema: makeMergedSchema({
     schemas,
     services: makeServices({ services }),
--- web/src/index.js
+++ web/src/index.js
@@ -1,3 +1,5 @@
+import { AuthProvider } from '@redwoodjs/auth'
+import netlifyIdentity from 'netlify-identity-widget'
 import ReactDOM from 'react-dom'
 import { RedwoodProvider, FatalErrorBoundary } from '@redwoodjs/web'
 import FatalErrorPage from 'src/pages/FatalErrorPage'
@@ -7,11 +9,19 @@ import Routes from 'src/Routes'
 import './scaffold.css'
 import './index.css'
 
+netlifyIdentity.init()
+
+netlifyIdentity.init()
+
 ReactDOM.render(
   <FatalErrorBoundary page={FatalErrorPage}>
-    <RedwoodProvider>
-      <Routes />
-    </RedwoodProvider>
+    <AuthProvider client={netlifyIdentity} type="netlify">
+      <AuthProvider client={netlifyIdentity} type="netlify">
+        <RedwoodProvider>
+          <Routes />
+        </RedwoodProvider>
+      </AuthProvider>
+    </AuthProvider>
   </FatalErrorBoundary>,
   document.getElementById('redwood-app')
 )
