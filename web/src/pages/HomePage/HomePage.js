4f9141ba811f4449dded0dd1190a9919269eabee
sunny yuen <yuens1002@gmail.com>
2020-07-12 08:58:17 -0400

    add a layout to wrap common components

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,9 @@
+import { Link, routes } from '@redwoodjs/router'
+
+import BlogLayout from 'src/layouts/BlogLayout/BlogLayout'
+
+const HomePage = () => {
+  return <BlogLayout>Home</BlogLayout>
+}
+
+export default HomePage
